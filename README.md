31828c9c957ad3b908ff5829a392b2f7033f0c45
jonasbn <jonasbn@logicLAB.dk>
2015-03-15 09:36:28 +0100

    First shot at working client, I do however want the client to act more like the DAS client and capture and present the result from the service, instead of this detaching behaviour

---
+++ README.md
@@ -0,0 +1,47 @@
+# NAME
+
+DK Hostmaster DSU service demo client
+
+# VERSION
+
+This documentation describes version 1.0.0
+
+# USAGE
+
+    $ morbo  client.pl
+
+Open your browser at:
+
+    http://127.0.0.1:3000/
+
+# DEPENDENCIES
+
+This client is implemented using Mojolicious::Lite in addition the following
+Perl modules are used all available from CPAN.
+
+- [Readonly](https://metacpan.org/pod/Readonly)
+- [Mojo::UserAgent](https://metacpan.org/pod/Mojo::UserAgent)
+- [Mojolicious::Plugin::ConsoleLogger](https://metacpan.org/pod/Mojolicious::Plugin::ConsoleLogger)
+
+In addition to the Perl modules, the client uses [Twitter Bootstrap](http://getbootstrap.com/) and hereby jQuery. These are automatically downloaded via CDNs and are not distributed with the client software.
+
+# SEE ALSO
+
+The main site for this client is the Github repository.
+
+- https://github.com/DK-Hostmaster/dsu-demo-client-mojolicious
+
+For information on the service, please refer to the documentation page with
+DK Hostmaster.
+
+- https://www.dk-hostmaster.dk/english/technical-administration/tech-notes/dnssec/dsu
+
+# COPYRIGHT
+
+This software is under copyright by DK Hostmaster A/S 2015
+
+# LICENSE
+
+This software is licensed under the MIT software license
+
+Please refer to the LICENSE file accompanying this file.
---
+++ client.pl
@@ -0,0 +1,260 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+
+our $VERSION = '1.0.0';
+
+my $endpoint = 'https://dsu-sandbox.dk-hostmaster.dk/1.0';
+
+my $algorithms = {
+    'RSA/SHA-256' => 8, 
+    'RSA/SHA-512' => 10,
+};
+my $digest_types = {
+    'SHA-1'   => 1, 
+    'SHA-256' => 2,
+};
+
+get '/' => sub {
+  my $self = shift;
+
+  my $params = $self->req->params->to_hash;
+
+  $self->render('index', 
+    version      => $VERSION,
+    domain       => 'test.dk',
+    userid       => 'TESTUSER-DK',
+    password     => 'supersecret',
+    params       => $params,
+    algorithms   => $algorithms,
+    digest_types => $digest_types,
+    endpoint     => $endpoint,
+  );
+};
+
+get '/prepare' => sub {
+  my $self = shift;
+
+  my $params = $self->req->params->to_hash;
+
+  if ($params->{action} and $params->{action} eq 'delete') {
+    $params->{'keytag.1'}    = 'DS_DELETE';
+    $params->{'algorithm.1'}  = 'DS_DELETE';
+    $params->{'digest_type.1'} = 'DS_DELETE';
+    $params->{'digest.1'}     = 'DS_DELETE';
+
+    # deleting navigational helper parameter
+    #delete $params->{'action'};
+
+    foreach my $param (grep !/(\w+\.1|domain|userid|password)/, keys %{$params}) {
+        delete $params->{$param};        
+    }
+  }
+
+  $self->render('prepare', 
+    version  => $VERSION,
+    params   => $params,
+    endpoint => $endpoint,
+  );
+};
+
+app->start;
+
+__DATA__
+
+@@prepare.html.ep
+% layout 'default';
+% title 'prepare';
+
+<form id="form" class="form-horizontal" role="form" action="/" method="GET" accept-charset="UTF-8">
+
+<div class="well well-lg">
+
+<!-- Key parameters -->
+% for my $number (1 .. 5) {
+
+    <!-- We only want keysets with all 4 parameters defined -->
+    % if ($params->{"keytag.$number"} and $params->{"digest.$number"} and $params->{"digest_type.$number"} and $params->{"algorithm.$number"}) {
+        % foreach my $param (grep /$number/, keys %{$params}) {
+            <code><%= $param %> = <%= $params->{$param} %></code><br/>
+            <input type="hidden" name="<%= $param %>" value="<%= $params->{$param} %>" />
+        % }
+    % }
+% }
+
+<!-- Non key parameters -->
+% foreach my $param (grep !/\w+\.\d+/, keys %{$params}) {
+    % if ($params->{$param}) {
+        <code><%= $param %> = <%= $params->{$param} %></code><br/>
+        <input type="hidden" name="<%= $param %>" value="<%= $params->{$param} %>" />
+    % }
+% }
+
+</div>
+
+<button id="send" type="button" name="send" id="send" class="btn btn-primary">Submit the request to: <%= $endpoint %> <span class="glyphicon glyphicon-send"></span></button>
+<button id="edit" type="submit" class="btn btn-default">Edit the request <span class="glyphicon glyphicon-wrench"></span></button>
+
+</form>
+
+@@option.html.ep
+<option value="<%= $value %>"><%= $key %></option>
+
+@@keyset.html.ep
+    <fieldset id="fieldset.<%= $number %>">
+    <legend>Keyset <%= $number %></legend>
+    <div class="form-group" style="width:96%;margin-left: auto;margin-right: auto;">
+        <!--<div class="control-group">-->
+            <div class="col-xs-2">
+            <% my $param = "keytag.$number"; %>
+            <label class="control-label" for="keytag">Keytag:</label>
+            <input name="keytag.<%= $number %>" id="keytag.<%= $number %>" class="form-control" placeholder="keytag" type="text" name="keytag" value="<%= $params->{$param} %>" />
+            </div>
+        <!--</div>-->
+        <!--<div class="control-group">-->
+            <div class="col-xs-6">
+            <% $param = "digest.$number"; %>
+            <label class="control-label" for="digest">Digest:</label>
+            <input name="digest.<%= $number %>" id="digest.<%= $number %>" class="form-control" placeholder="digest" type="text" name="digest" value="<%= $params->{$param} %>" />
+            </div>
+        <!--</div>-->
+        <!--<div class="control-group">-->
+            <div class="col-xs-2">
+            <% $param = "digest_type.$number"; %>
+            <label class="control-label" for="digest_type">Digest type:</label>
+            <select name="digest_type.<%= $number %>" id="digest_type.<%= $number %>" class="form-control">
+                % foreach my $digest_type (keys %{$digest_types}) {
+                %= include 'option', key => $digest_type, value => $digest_types->{$digest_type};
+                % }
+            </select>
+            </div>
+        <!--</div>-->
+        <!--<div class="control-group">-->
+            <div class="col-xs-2">
+            <% $param = "algorithm.$number"; %>
+            <label class="control-label" for="algorithm">Algorithm:</label>
+            <select name="algorithm.<%= $number %>" id="algorithm.<%= $number %>" class="form-control">
+                % foreach my $algorithm (keys %{$algorithms}) {
+                %= include 'option', key => $algorithm, value => $algorithms->{$algorithm};
+                % }
+            </select>
+            </div>
+        <!--</div>-->
+    </div>
+    </fieldset>
+
+@@ index.html.ep
+% layout 'default';
+% title 'index';
+
+    <form id="form" class="form-horizontal" role="form" action="/prepare" method="GET" accept-charset="UTF-8">
+    <!-- this hidden field is manipulated from JS (button clicks) -->
+    <input type="hidden" id="action" name="action" value="" />
+
+    <div class="form-group">
+        <div class="control-group">
+            <div class="col-xs-3">
+            <label class="control-label" for="domain.name">Domain name:</label>
+            <input id="domainname" class="form-control" placeholder="domain name" type="text" name="domain" value="<%= $domain %>" />
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="control-group">
+            <div class="col-xs-2">
+            <label class="control-label" for="userid">User-id:</label>
+            <input id="userid" class="form-control" placeholder="user-id" type="text" name="userid" value="<%= $userid %>" />
+            </div>
+        </div>
+    </div>
+
+    <!-- keysets -->
+    <div id="keysets">
+    % for my $keyset (1 .. 5) {
+    %= include 'keyset', number => $keyset, algorithms => $algorithms, digest_types => $digest_types
+    % }
+    </div>
+    <p>&nbsp;</p>
+    <p>
+    <button type="submit" id="prepare" class="btn btn-primary">Prepare addition request</button>
+    <button type="button" id="delete" name="delete" class="btn btn-danger">Prepare deletion request</button>
+    <button type="reset" class="btn btn-default">Reset to defaults</button>
+    <button type="button" id="clear" name="clear" class="btn btn-default">Clear</button>
+    </p>
+
+    </form>
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta .epp-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><%= title %></title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <!-- <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> -->
+      <!-- <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> -->
+    <![endif]-->
+  </head>
+  <body role="document">
+    <div class="container">
+    <!-- <a href="https://github.com/DK-Hostmaster/dsu-demo-client-mojolicious"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>-->
+    <h1>DK Hostmaster DSU service demo client - Version <%= $version %></h1>
+
+    <%= content %>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <script language="javascript">
+
+        // for handling proper reset (clear) of the form
+        function resetForm($form) {
+            // http://stackoverflow.com/questions/680241/resetting-a-multi-stage-form-with-jquery
+            $form.find('input:text, input:password, input:file, select textarea').val('');
+            $form.find('input:radio, input:checkbox')
+            .removeAttr('checked').removeAttr('selected');
+        };
+
+        // for handling actual submit to the endpoint
+        function send_to_endpoint() {
+            $('#form').attr("action", '<%= $endpoint %>');
+            $('#form').attr("method", 'POST'); 
+            $("#form").submit();
+        };
+
+        // for handling the click of the send button
+        $('#send').on('click', function() {
+                send_to_endpoint($('#form'));
+            }
+        );
+      
+        // for handling click of the delete button
+        $('#delete').on('click', function() {
+                $('#action').val('delete');
+                $("#form").submit();
+            }
+        );
+
+        // for handling click of the clear button
+        $('#clear').on('click', function() {
+                resetForm($('#form'));
+            }
+        );      
+    </script>
+
+  </body>
+</html>
